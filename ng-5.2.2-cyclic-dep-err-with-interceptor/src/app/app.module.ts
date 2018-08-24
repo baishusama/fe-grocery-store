a2efb7d3d7229a937e1a8b1ba7d4b56299945868
baishusama <baishusama@outlook.com>
2018-08-24 16:34:46 +0800

    #ng# finish v5.2.2 and v5.2.3 demos

--- ng-5.2.2-cyclic-dep-err-with-interceptor/src/app/app.module.ts
+++ ng-5.2.2-cyclic-dep-err-with-interceptor/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './auth.interceptor';
+import { AuthService } from './auth.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    AuthService, // ImoNote: 顺序看起来没啥影响？
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ng-5.2.2-cyclic-dep-err-with-interceptor/src/app/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+
+    if (token) {
+      req = req.clone({
+        headers: req.headers.set('Authorization', `Token ${token}`)
+      });
+    }
+
+    return next.handle(req);
+  }
+}
